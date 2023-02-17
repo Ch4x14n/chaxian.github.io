0dae954d4cf772cf56f21f3c272e24e7b67f2853
Ch4x14n <chaxian.contact@gmail.com>
2023-02-17 14:57:59 +0100

    .

---
+++ contact-btn.js
@@ -0,0 +1,4 @@
+const contactBtn = document.getElementById('contact-btn');
+contactBtn.addEventListener('click', function() {
+  // Insert your contact form code or redirect to a contact page here
+});
